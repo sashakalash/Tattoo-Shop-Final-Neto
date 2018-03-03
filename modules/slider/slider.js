62222ce60eaf709ce96921989c61528a725d0aa4
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2018-03-03 20:31:06 +0300

    changed slider image download logic

--- modules/slider/slider.js
+++ modules/slider/slider.js
@@ -1,5 +1,6 @@
 'use strict';
 const slider = document.querySelector('.slider');
+const sliderImg = slider.querySelector('img');
 const nextBtn = document.querySelector('.nextPhoto');
 const prevBtn = document.querySelector('.prevPhoto');
 const imgWideScreenBlock = document.querySelector('.imgPreview');
@@ -38,54 +39,60 @@ function slidePhoto(event) {
   const lastImg = preview.lastChild;
 
   let currentSlide = document.querySelector('.current_slide');
-  const nextSlide = currentSlide.nextElementSibling;
-  const prevSlide = currentSlide.previousElementSibling;
-  const firstSlide = slider.firstChild;
-  const lastSlide = slider.lastChild;
-
+  // const nextSlide = currentSlide.nextElementSibling;
+  // const prevSlide = currentSlide.previousElementSibling;
+  // const firstSlide = slider.firstChild;
+  // const lastSlide = slider.lastChild;
   currentSlide.classList.remove('current_slide');
   currentImg.classList.remove('img_choose');
 
   if (event.target.classList.contains('nextPhoto')) {
-    if (nextSlide) {
-      nextSlide.classList.add('current_slide');
+    if (nextImg) {
+      // nextSlide.classList.add('current_slide');
       nextImg.classList.add('img_choose');
+      sliderImg.src = nextImg.dataset.imgSrc;
     } else {
-      firstSlide.classList.add('current_slide');
+      // firstSlide.classList.add('current_slide');
       firstImg.classList.add('img_choose');
+      sliderImg.src = firstImg.dataset.imgSrc;
     }
   } else {
-    if (prevSlide) {
-      prevSlide.classList.add('current_slide');
+    if (prevImg) {
+      // prevSlide.classList.add('current_slide');
       prevImg.classList.add('img_choose');
+      sliderImg.src = prevImg.dataset.imgSrc;
     } else {
-      lastSlide.classList.add('current_slide');
+      // lastSlide.classList.add('current_slide');
       lastImg.classList.add('img_choose');
+      sliderImg.src = lastImg.dataset.imgSrc;
     }
   }
 }
 
-function createPreviewImgBlock() {
+function createPreviewImgBlock(evt) {
   if (imgData.responseText) {
     try {
-      const tattooImg = JSON.parse(imgData.responseText);
-      tattooImg.img.forEach((el, index) => {
-        const imgBlock = document.createElement('li');
-        const img = document.createElement('img');
-        img.src = el;
-        img.dataset.number = index;
-        imgBlock.appendChild(img);
-        slider.appendChild(imgBlock);
-      });
-      slider.firstChild.classList.add('current_slide');
+      const tattooImg = JSON.parse(evt.responseText);
+      // tattooImg.img.forEach((el, index) => {
+      //   const imgBlock = document.createElement('li');
+      //   const img = document.createElement('img');
+      //   img.src = el;
+      //   img.dataset.number = index;
+      //   imgBlock.appendChild(img);
+      //   slider.appendChild(imgBlock);
+      // });
+      // slider.firstChild.classList.add('current_slide');
       tattooImg.minis.forEach((el, index) => {
         const img = document.createElement('img');
         img.src = el;
-        img.dataset.number = index;
+        img.dataset.imgSrc = tattooImg.img[index];
         img.classList.add('previewImg');
         preview.appendChild(img);
       });
       preview.firstChild.classList.add('img_choose');
+      const img = slider.querySelector('img');
+      img.src = tattooImg.img[0];
+      img.classList.add('current_slide');
     } catch (e) {
       console.error(e.name, e.message);
     }
@@ -99,21 +106,22 @@ function errorReqData(err) {
 preview.addEventListener('click', showPreview);
 
 function showPreview(event) {
-  if (!event.target.classList.contains('previewImg')) {
+  if (!event.target.classList.contains('img_choose')) {
     return;
   }
   event.preventDefault();
-  const index = event.target.dataset.number;
-  chooseImg(index);
+  // const index = event.target.dataset.number;
+  chooseImg(event.target);
 }
 
-function chooseImg(index) {
+function chooseImg(previewImg) {
   const currentPreviewImg = preview.querySelector('.img_choose');
   currentPreviewImg.classList.remove('img_choose');
-  const currentSliderImg = slider.querySelector('.current_slide');
-  currentSliderImg.classList.remove('current_slide');
-  const choosedPreviewImg = preview.querySelector(`[data-number=${index}]`);
-  choosedPreviewImg.classList.add('img_choose');
-  const choosedSliderImg = slider.querySelector(`[data-number=${index}]`);
-  choosedSliderImg.classList.add('current_slide');
+  // const currentSliderImg = slider.querySelector('.current_slide');
+  // currentSliderImg.classList.remove('current_slide');
+  // const choosedPreviewImg = preview.querySelector(`[data-number=${index}]`);
+  previewImg.classList.add('img_choose');
+  sliderImg.src = previewImg.dataset.imgSrc;
+  // const choosedSliderImg = slider.querySelector(`[data-number=${index}]`);
+  // choosedSliderImg.classList.add('current_slide');
 }
